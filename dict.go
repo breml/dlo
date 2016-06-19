c4ebb0915c6e6042855ad7c396e87938b0c85b78
Lucas Bremgartner <lucas@bremis.ch>
2016-06-19 20:54:24 +0200

    Refactoring

--- dict.go
+++ dict.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"encoding/xml"
 	"fmt"
 	"io"
 	"io/ioutil"
+
+	"github.com/olekukonko/tablewriter"
 )
 
 type Doc struct {
@@ -28,14 +32,38 @@ type Side struct {
 	Word string `xml:"words>word"`
 }
 
-func ProcessQueryXml(input io.Reader) (Doc, error) {
+func ProcessQueryXml(input io.Reader) (*Doc, error) {
 	b, err := ioutil.ReadAll(input)
 	if err != nil {
-		return Doc{}, fmt.Errorf("read resp body error: %s", err)
+		return nil, fmt.Errorf("read resp body error: %s", err)
 	}
 
 	var d Doc
 	xml.Unmarshal(b, &d)
 
-	return d, nil
+	return &d, nil
+}
+
+func (d Doc) String() string {
+	var buf bytes.Buffer
+	w := bufio.NewWriter(&buf)
+	r := bufio.NewReader(&buf)
+	// for _, s := range d.Sections {
+	for i := len(d.Sections) - 1; i >= 0; i-- {
+		s := d.Sections[i]
+		table := tablewriter.NewWriter(w)
+		table.SetAutoWrapText(false)
+		table.SetHeader([]string{"En", "De"})
+		for _, v := range s.Entries {
+			table.Append([]string{v.Sides[0].Word, v.Sides[1].Word})
+		}
+		fmt.Fprintln(w, s.SectionName)
+		table.Render()
+	}
+	w.Flush()
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		return ""
+	}
+	return string(b)
 }
--- main.go
+++ main.go
@@ -5,33 +5,29 @@ import (
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/olekukonko/tablewriter"
 )
 
 func main() {
+	d, err := search(os.Args)
+	if err != nil {
+		log.Fatalf("search failed: %s", err)
+	}
+	fmt.Print(d)
+}
+
+func search(args []string) (*Doc, error) {
+	if len(args) < 2 {
+		return nil, fmt.Errorf("Minimum 1 arg required")
+	}
+
 	// Arguments
 	// rmWords=off -> entfernt Words aus der XML-Antwort
-	resp, err := http.Get("http://dict.leo.org/dictQuery/m-vocab/ende/query.xml?tolerMode=nof&lp=ende&lang=de&rmWords=off&rmSearch=on&search=" + os.Args[1] + "&searchLoc=0&resultOrder=basic&multiwordShowSingle=on&pos=0&sectLenMax=16&n=1")
+	// sectLenMax=10 -> search results per section
+	resp, err := http.Get("http://dict.leo.org/dictQuery/m-vocab/ende/query.xml?tolerMode=nof&lp=ende&lang=de&rmWords=off&rmSearch=on&search=" + os.Args[1] + "&searchLoc=0&resultOrder=basic&multiwordShowSingle=on&pos=0&sectLenMax=10&n=1")
 	if err != nil {
 		log.Fatalf("http get error: %s", err)
 	}
 	defer resp.Body.Close()
 
-	d, err := ProcessQueryXml(resp.Body)
-
-	// fmt.Println(d)
-
-	// for _, s := range d.Sections {
-	for i := len(d.Sections) - 1; i >= 0; i-- {
-		s := d.Sections[i]
-		table := tablewriter.NewWriter(os.Stdout)
-		table.SetAutoWrapText(false)
-		table.SetHeader([]string{"En", "De"})
-		for _, v := range s.Entries {
-			table.Append([]string{v.Sides[0].Word, v.Sides[1].Word})
-		}
-		fmt.Println(s.SectionName)
-		table.Render() // Send output
-	}
+	return ProcessQueryXml(resp.Body)
 }
